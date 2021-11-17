cd5227b2c6a09b8969ec08f0a66f53572aede457
yihong0906 <tehyh-wm18@student.tarc.edu.my>
2021-11-17 18:46:35 +0800

    update home.py
    description

--- apps/home.py
+++ apps/home.py
@@ -19,10 +19,10 @@ def app():
     st.caption('• Send real-time notification if crowd is detected')
 
     st.subheader('Human Action Recognition Detector')
-    st.caption('• Detect humans in the frame with yolov3')
-    st.caption('• Estimate number of people who violate social distance')
-    st.caption('• Create trend chart of social distancing violations')
-    st.caption('• Send real-time notification if crowd is detected')
+    st.caption('• Detect humans in the frame with tensorflow openpose')
+    st.caption('• Estimate the abnormal behaviour')
+    st.caption('• Create trend chart of human actions')
+    st.caption('• Send real-time notification if abnormal behaviour occurs')
 
     expander_faq = st.expander("More About Our Project")
     expander_faq.write("Hi there! If you have any questions about our project, or simply want to check out the source code, please visit our github repo: https://github.com/chinyew24/Smart-Crowd-Monitoring-System")
